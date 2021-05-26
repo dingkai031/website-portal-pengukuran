3d616d615d5f9badb97dc52ff0b4b375b7ba7f13
Yovan Julio Adam <yovanjulioadam@gmail.com>
2021-05-26 15:09:03 +0700

    paling bener

--- Inspeksi/assets/js/main.js
+++ Inspeksi/assets/js/main.js
@@ -67,12 +67,12 @@ tambahField("jamkerja");
 showNavbar("header-toggle", "nav-bar", "body-pd", "header");
 
 /*===== LINK ACTIVE  =====*/
-const linkColor = document.querySelectorAll(".nav__link");
+// const linkColor = document.querySelectorAll(".nav__link");
 
-function colorLink() {
-  if (linkColor) {
-    linkColor.forEach(l => l.classList.remove("active"));
-    this.classList.add("active");
-  }
-}
-linkColor.forEach(l => l.addEventListener("click", colorLink));
+// function colorLink() {
+//   if (linkColor) {
+//     linkColor.forEach(l => l.classList.remove("active"));
+//     this.classList.add("active");
+//   }
+// }
+// linkColor.forEach(l => l.addEventListener("click", colorLink));
